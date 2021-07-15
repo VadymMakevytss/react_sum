8642a3157f2f553819506fdd80643dc520bf4012
VadymMakevytss <vadymlat@mail.ru>
2021-07-15 12:23:46 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.26964668c8948041e40a67af3027bb85.js"
 );
 
 self.addEventListener('message', (event) => {
